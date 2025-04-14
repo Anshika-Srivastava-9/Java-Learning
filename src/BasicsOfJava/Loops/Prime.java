1ba774fc9ad9b700917313989298352c80f81553
Anshika <srianshika89@gmail.com>
2025-04-14 16:40:53 +0530

    My java

---
+++ src/BasicsOfJava/Loops/Prime.java
@@ -0,0 +1,22 @@
+package BasicsOfJava.Loops;
+import java.util.Scanner;
+public class Prime {
+    public static void main(String[] args){
+        Scanner sc= new Scanner(System.in);
+        System.out.println("Enter the number:");
+        int n=sc.nextInt();
+        boolean flag = false;
+        for(int i=2;i<n;i++){
+            if(n%i==0){
+                flag = true;
+                break;
+            }
+        }
+      if(n==1)
+      System.out.println("Niether prime nor composite");
+      else if(flag==false)
+          System.out.println("Prime number");
+        else
+          System.out.println("Composite Number");
+    }
+}
