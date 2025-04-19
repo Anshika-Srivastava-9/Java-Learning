5b181671875c11b6770453284c93ab0c99445ca7
Anshika <srianshika89@gmail.com>
2025-04-19 20:40:59 +0530

    My java

---
+++ src/BasicsOfJava/Loops/Pattern.java
@@ -0,0 +1,62 @@
+package BasicsOfJava.Loops;
+
+import java.util.Scanner;
+public class Pattern {
+    public static void main(String [] args){
+
+        //SUM OF ALL DIGIT OF GIVEN NUMBER//
+//    -------------------------------------------
+//        Scanner sc=new Scanner(System.in);
+//        int n=sc.nextInt();
+//        int sum=0;
+//        while(n!=0){
+//            int ld = n%10;  //This will give remainder
+//            sum+=ld;
+//            n/=10;          // This will remove last number
+//  }
+//           System.out.println(sum);
+
+
+           //PRODUCT OF ALL DIGIT OF GIVEN NUMBER//
+//      ----------------------------------------------
+//        Scanner sc=new Scanner(System.in);
+//        int n=sc.nextInt();
+//        int product=1;
+//        while(n!=0){
+//            int ld = n%10;  //This will give remainder
+//            product*=ld;
+//            n/=10;          // This will remove last number
+//        }
+//        System.out.println(product);
+
+
+               //PRODUCT OF NON-ZERO NUMBERS//
+//        -------------------------------------------
+//        Scanner sc = new Scanner(System.in);
+//        int n = sc.nextInt();
+//        int product = 1;
+//        while (n != 0) {
+//            int ld = n % 10;      // last digit
+//            if (ld == 0) {        // skip zero digits
+//                n /= 10;          // remove that digit
+//                continue;
+//            }
+//            product *= ld;        // multiply by non-zero digit
+//            n /= 10;              // remove that digit
+//        }
+//
+//        System.out.println(product);
+
+        //SUM OF ALL EVEN DIGIT OF GIVEN NUMBER//
+//  -------------------------------------------------
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int sum=0;
+        while(n!=0){
+            int ld=n%10;
+
+
+
+        }
+    }
+}
