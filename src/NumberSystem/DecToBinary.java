b6f746c7b545517f7f84eb165e12f6835e1169f0
Anshika-Srivastava-9 <srianshika89@gmail.com>
2025-05-26 20:14:50 +0530

    Basic java

---
+++ src/NumberSystem/DecToBinary.java
@@ -0,0 +1,11 @@
+package NumberSystem;
+import java.util.Scanner;
+public class DecToBinary {
+    public static void main(String []arg){
+      Scanner sc=new Scanner (System.in);
+      int decNum=sc.nextInt();
+      int ans=0;
+      int power=1;
+
+    }
+}
